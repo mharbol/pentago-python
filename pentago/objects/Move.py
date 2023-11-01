17226d9f54dd443770c99b69369e00f54c0555b0
Marcus Harbol <mharbol@users.noreply.github.com>
2023-10-31 23:15:28 -0400

    more content to Move

--- pentago/objects/Move.py
+++ pentago/objects/Move.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from enum import Enum
 
 class Direction(Enum):
@@ -12,6 +13,7 @@ class Direction(Enum):
 
 class Move:
     def __init__(self, row_number : int, col_number : int, quadrant : int, direction : Direction) -> None:
+        # TODO out of bounds checks
         self.row_number = row_number
         self.col_number = col_number
         self.quadrant = quadrant
@@ -25,5 +27,29 @@ class Move:
         """
         return (self.row_number << 6) | (self.col_number << 3) | (self.quadrant << 1) | int(self.direction)
 
+    def __eq__(self, o : object) -> bool:
+        if type(o) != type(self):
+            return False
+        return self.row_number == o.row_number and self.col_number == o.col_number and \
+            self.quadrant == o.quadrant and self.direction == o.direction
+
+    def __neq__(self, o : object) -> bool:
+        return not self == o
+
+    def __str__(self) -> str:
+        return f"Row: ${self.row_number}, Column: ${self.col_number}, Quadrant: ${self.quadrant}," + \
+            f"Direction: ${self.direction}"
+
+    def __repr__(self) -> str:
+        return str(self)
+
     def __int__(self) -> int:
         return hash(self)
+
+    @staticmethod
+    def unhash(move_repr : int) -> Move:
+        row : int = move_repr >> 6
+        col : int = move_repr >> 3 & 0b111
+        dir : int = move_repr & 0b1
+        quad : int = move_repr >> 1 & 0b11
+        return Move(row, col, quad, dir)
